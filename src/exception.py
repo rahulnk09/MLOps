ebac32fbbe35f13d51dfc7e0af2cba78377a0275
Rahul Nakka <rahul.nakka02@gmail.com>
2024-07-25 08:42:26 +0530

    Project Structure updated

--- src/exception.py
+++ src/exception.py
@@ -17,9 +17,3 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_message
 
-if __name__=='__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Division by zero')
-        raise CustomException(e,sys)
